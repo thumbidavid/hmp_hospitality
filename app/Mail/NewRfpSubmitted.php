a7df3e143f55547eff0917311e216b43da4e4306
David Thumbi <thumbidavid78@gmail.com>
2026-09-24 09:27:09 +0300

    feat: RFP email notifications for admin and client confirmation

--- app/Mail/NewRfpSubmitted.php
+++ app/Mail/NewRfpSubmitted.php
@@ -11,7 +11,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class NewRfpSubmitted extends Mailable implements ShouldQueue
+class NewRfpSubmitted extends Mailable
 {
     use Queueable, SerializesModels;
 
--- app/Mail/RfpClientConfirmation.php
+++ app/Mail/RfpClientConfirmation.php
@@ -11,7 +11,7 @@ use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
-class RfpClientConfirmation extends Mailable implements ShouldQueue
+class RfpClientConfirmation extends Mailable
 {
     use Queueable, SerializesModels;
 
